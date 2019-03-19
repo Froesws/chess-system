564545bd9c5d74323c52dcb4e86e1be723f79359
William Froes <froes.ws@gmail.com>
2019-03-19 13:51:04 -0300

    Position added

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position pos = new Position (7,8);
+		
+		System.out.println(pos);
+
+	}
+
+}
